c954a97ca968bbe3563fd51e02dc7b3b706f425b
zryuik <yuristronda123@hotmail.com>
2025-11-28 21:37:44 -0300

    aprendendo a usar operadores aritmeticos em java

---
+++ exs_logica/02.java
@@ -0,0 +1,22 @@
+//somando em Java
+
+public class Main {
+    public static void main(String[] args) {
+        // criando variáveis
+        int num1 = 5;
+        int num2 = 10;
+
+        // somando os valores
+        int soma = num1 + num2;
+        int dim = num1 - num2;
+        int multi = num1 * num2;
+        int div = num1 * num2;
+        
+        System.out.println("A soma entre " + num1 + " e " + num2 + " é " + soma);
+        System.out.println("A diminuição entre " + num1 + " e " + num2 + " é " + dim);
+        System.out.println("A multiplicação entre " + num1 + " e " + num2 + " é " + multi);
+        System.out.println("A divisão entre " + num1 + " e " + num2 + " é " + div);
+        System.out.println("========= FIM! =========");
+        
+    }
+}
